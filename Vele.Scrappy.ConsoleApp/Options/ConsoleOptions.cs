d1bc183f5b47a0051a4f0534033253f7e1e6e8e1
Mateo Velenik <mateo.velenik@gmail.com>
2021-11-26 15:18:48 +0100

    implemented command line options parsing

---
+++ Vele.Scrappy.ConsoleApp/Options/ConsoleOptions.cs
@@ -0,0 +1,24 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vele.Scrappy.ConsoleApp.Options
+{
+    public class ConsoleOptions
+    {
+        [Option('o', "output-folder", Required = true, HelpText = "Set output folder to put the website in.")]
+        public string OutputFolder { get; set; }
+
+        [Option('u', "website-url", Required = true, HelpText = "Set URL from which to scrape the website from.")]
+        public string WebsiteUrl { get; set; }
+
+        [Option('e', "download-external", Required = false, Default = false, HelpText = "Determine whether to download external files or not.")]
+        public bool DownloadExternal { get; set; }
+
+        [Option('p', "parallel-execution", Required = false, Default = 4, HelpText = "Determine the amount of parallel processing tasks.")]
+        public int ParallelExecutionCount { get; set; }
+    }
+}
--- Vele.Scrappy.ConsoleApp/Program.cs
+++ Vele.Scrappy.ConsoleApp/Program.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using CommandLine;
+using Serilog;
+using Serilog.Core;
+using System;
+using System.Collections.Generic;
+using Vele.Scrappy.ConsoleApp.Options;
 
 namespace Vele.Scrappy.ConsoleApp
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        private static Logger _logger;
+
+        public static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _logger = new LoggerConfiguration()
+                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss.fff}] {Message:lj}{NewLine}")
+                .CreateLogger();
+
+            int returnCode = Parser.Default.ParseArguments<ConsoleOptions>(args)
+                .MapResult(
+                    (ConsoleOptions opts) => Run(opts),
+                    (IEnumerable<Error> errs) => HandleErrors(errs));
+
+            _logger.Dispose();
+
+            return returnCode;
+        }
+
+        private static int HandleErrors(IEnumerable<Error> errs)
+        {
+            return -1;
+        }
+
+        private static int Run(ConsoleOptions opts)
+        {
+            _logger.Information($"Starting scraping process for '{opts.WebsiteUrl}'...");
+
+            try
+            {
+                // TODO: invoke scraping process here!
+
+                _logger.Information("Scraping completed!");
+                return 0;
+            }
+            catch (AggregateException aggrEx)
+            {
+                _logger.Error($"Exception occured: {aggrEx.InnerException ?? aggrEx}");
+                return -1;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Exception occured: {ex}");
+                return -1;
+            }
         }
     }
 }
